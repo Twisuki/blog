2cf120b6dc5fe19610fda3727dd6a983b6ffa877
Twisuki <Twisuki@outlook.com>
2025-04-13 23:31:49 +0800

    删除back to top配置项

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -175,10 +175,6 @@ export default hopeTheme(
 				indexName: "twis",
 			},
 
-			backToTop:  {
-				threshold: 500
-			}
-
 			// 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
 			// pwa: {
 			//   favicon: "/favicon.ico",
