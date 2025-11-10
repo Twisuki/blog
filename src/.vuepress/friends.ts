0f3751b9ef75306812e52a937fafd68c53d08788
Twisuki <Twisuki@outlook.com>
2025-11-10 12:41:05 +0800

    :sparkles: 添加友链

--- src/.vuepress/friends.ts
+++ src/.vuepress/friends.ts
@@ -1,71 +1,77 @@
-import {ThemeBlogHomeProjectOptions} from 'vuepress-theme-hope'
+import { ThemeBlogHomeProjectOptions } from 'vuepress-theme-hope'
 
 const friends: ThemeBlogHomeProjectOptions[] = [
-	{
-		name: "SilverAg.L",
-		icon: "https://nyacl.shimakaze.org/assets/images/avatar.webp",
-		desc: "喜欢涩涩的，（并不）可爱的小魅魔。十分珍重与氯喵的纠缠之缘。",
-		link: "https://nyacl.shimakaze.org/",
-	},
-	{
-		name: "Snowykami 神羽",
-		icon: "https://github.com/Snowykami.png",
-		desc: "Redrock 后端人员, 主要维护轻雪的相关项, 时不时在 MC 里整活. ",
-		link: "https://sfkm.me/"
-	},
-	{
-		name: "ErrOrO's Space",
-		icon: "https://blog.erroro.moe/assets/avatar.jpg",
-		desc: "OrO?OrO!",
-		link: "https://blog.erroro.moe"
-	},
-	{
-		name: "Dislink Sforza 断联",
-		icon: "https://github.com/Dislink.png",
-		desc: "Undefined",
-		link: "https://github.com/Dislink"
-	},
-	{
-		name: "Ariasaka",
-		icon: "https://bu.dusays.com/2024/12/05/67517bcf104da.png",
-		desc: "人有悲欢离合 月有阴晴圆缺",
-		link: "https://blog.yaria.top"
-	},
-	{
-		name: "yuhan2680 小涵",
-		icon: "/friends/yuhan2680.jpg",
-		desc: "你好, 这里是小涵w",
-		link: "https://github.com/yuhan2680"
-	},
-	{
-		name: "ChenXu233 沐浴晨煦",
-		icon: "https://github.com/ChenXu233.png",
-		desc: "Description !!",
-		link: "https://github.com/ChenXu233"
-	},
-	{
-		name: "BFladderbean",
-		icon: "/friends/BFladderbean.jpg",
-		desc: "OIer(AFOed)/Front-End/Back-End/Machine Learning. C++/Go/JS/TS/Python",
-		link: "https://github.com/BFladderbean"
-	},
-	{
-		name: "青稚",
-		icon: "https://blog.linux-qitong.top/img/avatar.webp",
-		desc: "青稚の空间 - 越努力，越幸运.",
-		link: "https://blog.linux-qitong.top",
-	},
-	{
-		name: "Pinpe",
-		icon: "https://pinpe.top/head.jpg",
-		desc: "一个属于自己的云朵。",
-		link: "https://pinpe.top",
-	},
-	{
-		name: "SD7935",
-		icon: "https://q1.qlogo.cn/g?b=qq&s=640&nk=2691681405",
-		desc: "一个CTFer、红警牢玩家、死宅、蟒蛇使用者、hentai",
-		link: "https://www.sd7935.link/",
-	}
+  {
+    name: "Snowykami 神羽",
+    icon: "https://github.com/Snowykami.png",
+    desc: "Redrock 后端人员, 主要维护轻雪的相关项, 时不时在 MC 里整活. ",
+    link: "https://sfkm.me/"
+  },
+  {
+    name: "ErrOrO's Space",
+    icon: "https://blog.erroro.moe/assets/avatar.jpg",
+    desc: "OrO?OrO!",
+    link: "https://blog.erroro.moe"
+  },
+  {
+    name: "SilverAg.L",
+    icon: "https://nyacl.shimakaze.org/assets/images/avatar.webp",
+    desc: "喜欢涩涩的，（并不）可爱的小魅魔。十分珍重与氯喵的纠缠之缘。",
+    link: "https://nyacl.shimakaze.org/",
+  },
+  {
+    name: "Dislink Sforza 断联",
+    icon: "https://github.com/Dislink.png",
+    desc: "Undefined",
+    link: "https://github.com/Dislink"
+  },
+  {
+    name: "Ariasaka",
+    icon: "https://bu.dusays.com/2024/12/05/67517bcf104da.png",
+    desc: "人有悲欢离合 月有阴晴圆缺",
+    link: "https://blog.yaria.top"
+  },
+  {
+    name: "yuhan2680 小涵",
+    icon: "/friends/yuhan2680.jpg",
+    desc: "你好, 这里是小涵w",
+    link: "https://github.com/yuhan2680"
+  },
+  {
+    name: "ChenXu233 沐浴晨煦",
+    icon: "https://github.com/ChenXu233.png",
+    desc: "Description !!",
+    link: "https://github.com/ChenXu233"
+  },
+  {
+    name: "BFladderbean",
+    icon: "/friends/BFladderbean.jpg",
+    desc: "OIer(AFOed)/Front-End/Back-End/Machine Learning. C++/Go/JS/TS/Python",
+    link: "https://github.com/BFladderbean"
+  },
+  {
+    name: "青稚",
+    icon: "https://blog.linux-qitong.top/img/avatar.webp",
+    desc: "青稚の空间 - 越努力，越幸运.",
+    link: "https://blog.linux-qitong.top",
+  },
+  {
+    name: "Pinpe",
+    icon: "https://pinpe.top/head.jpg",
+    desc: "一个属于自己的云朵。",
+    link: "https://pinpe.top",
+  },
+  {
+    name: "SD7935",
+    icon: "https://q1.qlogo.cn/g?b=qq&s=640&nk=2691681405",
+    desc: "一个CTFer、红警牢玩家、死宅、蟒蛇使用者、hentai",
+    link: "https://www.sd7935.link/",
+  },
+  {
+    name: "阿叶Ayeez",
+    icon: "https://blog.ayeez.cn/imgs/photo.jpg",
+    desc: "记录学习历程, 记录美好生活",
+    link: "https://blog.Ayeez.cn",
+  }
 ]
 export default friends
